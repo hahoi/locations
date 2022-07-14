5acc4fda08898a6a6880cca1c583369effa5b2c6
haho <haho@hotmail.com>
2022-07-14 10:11:16 +0800

    2022-07-14-1000

--- src/components/search.vue
+++ src/components/search.vue
@@ -7,16 +7,23 @@
     >
       <q-input
         clearable
+        borderless
         color="secondary"
         v-model="searchField"
         label="關鍵字查詢，多個關鍵字用空白隔開"
         @keyup.esc="searchField = ''"
         outlined
-        class=""
-        style="border: 1px solid #33ff00; border-radius: 5px"
+        class="text-green"
+        style="
+          font-size: 20px;
+          color: #33ff00;
+          border: 3px solid #33ff00;
+          border-radius: 5px;
+        "
       />
     </transition>
   </div>
+  <!-- style="" -->
 </template>
 
 <script setup>
@@ -39,5 +46,8 @@ const searchField = computed({
 <style>
 button.q-icon.notranslate.material-icons.q-field__focusable-action {
   color: red;
+}
+#f_0f59587e-af72-467a-ac56-4f8883791b6b {
+  color: green;
 }
 </style>
--- src/pages/IndexPage.vue
+++ src/pages/IndexPage.vue
@@ -1,11 +1,60 @@
 <template>
   <q-page class="">
-    <div class="text-center q-pa-md" style="position: relative; height: 80vh">
-      <div><h5>親子對好玩公園的搜尋需求</h5></div>
-      <div><h5>搜尋引擎如何解決問題</h5></div>
-      <div><h5>可複製，未來能延伸到全國各點特色公園</h5></div>
+    <div
+      class="text-center"
+      style="position: relative; height: 80vh; max-width: 800px; margin: 0 auto"
+    >
+      <div class="text-h5 q-ma-md">現代孩子打發時間方式</div>
 
-      <q-parallax :height="150">
+      <div class="row justify-center" style="position: relative">
+        <div
+          class="col-sm-6 col-xs-12"
+          style="
+            background-image: url(/img/img4.jpg);
+            background-position: center;
+            background-size: contain;
+            background-repeat: no-repeat;
+            filter: brightness(30%);
+          "
+        ></div>
+        <div
+          class="col-sm-6 col-xs-12"
+          style="
+            background-image: url(/img/img5.jpg);
+            background-position: center;
+            background-size: contain;
+            background-repeat: no-repeat;
+            filter: brightness(50%);
+          "
+        ></div>
+        <div
+          class="row text-center text-h5 text-white full-height items-center justify-center"
+          style="position: absolute; top: 10px"
+        >
+          <p class="q-ma-md col-xs-12 col-sm-4">缺少活動</p>
+          <p class="q-ma-md col-xs-12 col-sm-4">視力受到影響</p>
+          <p class="q-ma-md col-xs-12 col-sm-4">沉迷3C世界</p>
+        </div>
+      </div>
+
+      <div class="q-ma-md">
+        <span class="text-h5">孩子需要的是</span>
+        <span class="text-h4">奔跑</span>
+      </div>
+      <div
+        class="col-sm-12 col-xs-12"
+        style="
+          background-image: url(/img/img5.jpg);
+          background-position: center;
+          background-size: contain;
+          background-repeat: no-repeat;
+          filter: brightness(50%);
+        "
+      >
+        <div><h5>搜尋引擎如何解決問題</h5></div>
+        <div><h5>可複製，未來能延伸到全國各點特色公園</h5></div>
+
+        <!-- <q-parallax :height="150">
         <template v-slot:media>
           <video
             width="720"
@@ -27,7 +76,8 @@
         </template>
 
         <h3 class="text-white">Video</h3>
-      </q-parallax>
+      </q-parallax> -->
+      </div>
     </div>
     <!-- 回到頂端 -->
 
@@ -58,3 +108,12 @@ export default defineComponent({
   },
 });
 </script>
+<style lang="sass" scoped>
+.row > div
+  padding: 10px 15px
+  height: 20vh
+.row + .row
+  margin-top: 1rem
+</style>
+<!-- background: rgba(86,61,124,.15) -->
+<!-- border: 1px solid rgba(86,61,124,.2) -->
